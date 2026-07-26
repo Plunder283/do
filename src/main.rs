88afa3928d934e2d75534208610acc5a61c7f9cc
mateo.ziegler <mateolziegler@gmail.com>
2026-07-26 23:57:34 +0200

    blah blah blahh

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,31 @@
 use std::env;
 use std::process::Command;
 
+fn launch_editor(file: &str) {
+    let status = Command::new("vim")
+            .arg(format!("~/.local/share/task/{file}"))
+            .status()
+            .expect("Impossible to run vim");
+
+    if status.success() {
+        println!("Your notes had been saved!");
+    } else {
+        println!("An error occurred while writing your notes");
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     
     if args.len() == 1 {
-        return; // Faire plus tard
+        launch_editor(todo);
+        return;
     }
 
-    if args[1] == "note" || args[1] == "" {
-        let status = Command::new("vim")
-            .arg("~/.local/share/task/note.txt")
-            .status()
-            .expect("Impossible to run vim");
+    if args[1] == "note" || args[1] == "draft" {
+        launch_editor("draft.txt");
+        return;
+    } else if args[1] == "add" {
 
-        if status.success() {
-            println!("Your notes had been saved!");
-        } else {
-            println!("An error occurred while writing your notes");
-        }
     }
 }
